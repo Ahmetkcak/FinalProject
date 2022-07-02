22bd8208b2f540d2347b052d2873754df8f50e27
AHMET KOCAK <f201213038@ktun.edu.tr>
2022-07-02 14:34:48 +0300

    2.07.2022

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+
+    // Generic constraint
+    // class: referans tip 
+    //IEntity: IEntity olabilir yada IEntity implemente eden bir sınıf olabilir
+    //new: new'lenebilir olmalı.IEntity bir interface olduğundan new'lenemez bu yüzden de interfacei implement eden sınıfları kullan demiş oluruz
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Udate(T entity);
+
+        void Delete(T entity);
+
+    }
+}
